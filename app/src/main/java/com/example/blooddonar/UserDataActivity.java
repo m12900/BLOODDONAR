16eeadf3eb8924557f999fbd58c456771f49e7d1
Muhammad Abdullah <ystass2019@gmail.com>
2021-08-25 09:12:23 +0500

    BLOODDONAR

---
+++ app/src/main/java/com/example/blooddonar/UserDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.blooddonar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_data);
+    }
+}
